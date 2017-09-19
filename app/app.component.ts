d7c69b99ec9874ef789ba0a93fa8af8ec9778175
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-09-19 20:09:17 +0200

    01 Firs Component
    Como crear, declarar, importar y exportar  un Componente Base para una App

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+// Necesitamos 'import' la declaración de 'Component' del Core de Angular
+import { Component } from "@angular/core";
 
+/**
+ * Necesitamos exportar nuestro nuevo 'Component' con una 'class'
+ * Componente Root.
+ * @export
+ * @class AppComponent
+ */
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Hello!
-    </div>
-  `
+	selector: 'app-root', // <app-root></app-root>
+	styleUrls: ['app.component.scss'], // referencia al sass
+	template: `
+		<div class="app">
+			{{ title }}
+		</div>
+	`
 })
 export class AppComponent {
-
+	/**
+	 * Declaramos las varibles y le decimos el tipo de dato.
+	 * @type {string}
+	 * @memberof AppComponent
+	 */
+	title: string;
+	/**
+	 * Crea una instanctia de AppComponent.
+	 * @memberof AppComponent
+	 */
+	constructor() {
+		this.title = 'Ultimate Angular Fundamentals'; 
+	}
 }
