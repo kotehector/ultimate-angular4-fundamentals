b90b5989168b00e2554f993b02c328cbe604b136
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-09-20 19:29:33 +0200

    Root Module con @NgModule
    Crear el Módulo raíz con @NgModule con sus declaraciones, imports y el Componente para bootstrap la App

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,8 @@
-// Necesitamos 'import' la declaración de 'Component' del Core de Angular
 import { Component } from "@angular/core";
 
-/**
- * Necesitamos exportar nuestro nuevo 'Component' con una 'class'
- * Componente Root.
- * @export
- * @class AppComponent
- */
 @Component({
-	selector: 'app-root', // <app-root></app-root>
-	styleUrls: ['app.component.scss'], // referencia al sass
+	selector: 'app-root', 
+	styleUrls: ['app.component.scss'], 
 	template: `
 		<div class="app">
 			{{ title }}
@@ -17,16 +10,8 @@ import { Component } from "@angular/core";
 	`
 })
 export class AppComponent {
-	/**
-	 * Declaramos las varibles y le decimos el tipo de dato.
-	 * @type {string}
-	 * @memberof AppComponent
-	 */
 	title: string;
-	/**
-	 * Crea una instanctia de AppComponent.
-	 * @memberof AppComponent
-	 */
+	
 	constructor() {
 		this.title = 'Ultimate Angular Fundamentals'; 
 	}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+/**
+ * Esto son los 3 módulos principales que necesitamos importar para
+ * una App Base en Angular
+ */
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
 
-import { AppComponent } from './app.component';
+// importamos el Componente que va a ser el Root de este Módulo (App)
+import { AppComponent } from "./app.component";
 
+/**
+ * Un Módulo es un contenedor que contiene todos los Componentes que
+ * necesitemos y todas sus dependencias (servicios)
+ */
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
-  declarations: [
-    AppComponent
-  ]
+	// Declaración de Componentes que vamos a necesitar en la App
+	declarations: [
+		AppComponent 
+	],
+	// imports de Angular que vamos a utilizar
+	imports: [
+		BrowserModule,
+		CommonModule
+	],
+	bootstrap: [ AppComponent ]		// Componente raíz
 })
-export class AppModule {}
+ export class AppModule {}
