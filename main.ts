886179fbf4e4fa4d5fe2ea04e58676628a2006e8
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-09-20 19:47:50 +0200

    03 Bootstrapping Angular
    Proceso final de Bootstraping de la App. Explicación del flujo para arrancar.

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+/**
+ *  Necesitamos esta parte de Angular para decirle a la App que utilice
+ *  este componente para compilar la App dinámicamente en el navegador.
+ */
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
+// Necesitamos importar el Módulo Raíz.
+import { AppModule } from "./app/app.module";
 
+// Lanzamos el Compilador
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+/**
+ * Los pasos a seguir son:
+ * 	1 - Creamos el app.component (tag, estilos, template)
+ * 	2 - Creamos el app.module (delcaracion, imports y bootstrap)
+ * 	3 - Hacemos el Bootstrapping desde el main.ts 
+ */
