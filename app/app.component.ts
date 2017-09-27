141796ddc646eb35daf9c2bc49383dc8703692e3
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-09-27 19:33:59 +0200

    10 - ngFor and iterating collections
    Explicación y uso de *ngFor.
    Explicación y uso de Interface en TS

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,41 +1,74 @@
 import { Component } from "@angular/core";
 
+/**
+ * Interface para definir el tipo de Dato Personalizado
+ * con las restricciones de Datos para TS
+ * 
+ * @interface Passenger
+ */
+interface Passenger {
+	id: number,
+	fullname: string,
+	checkedIn: boolean
+}
+
 @Component({
   selector: "app-root",
   styleUrls: ["app.component.scss"],
   template: `
 		<div class="app">
 			
-			<input 
-				type="text" 
-				[value]="name"
-				(input)="handleChange($event.target.value)">
-			
-			<!-- comportamiento del '*' internamente de Angular -->
-			<template [ngIf]="name.length > 2">
-				<div>
-					Buscando ... {{ name }}</div>
-				</div>
+		<h3>Pasajeros (explicación)</h3>
+		<ul>
+			<!-- Explicación del ngFor -->
+			<template ngFor let-passenger let-i="index" [ngForOf]="passengers">
+				<li>
+					{{ i }}: {{ passenger.fullname }}
+				</li>
 			</template>
+		</ul>
 
-			<!-- usamos el '*' para simplificar (sugar sintax) -->
-			<div *ngIf="name.length > 2">
-				Buscando ... {{ name }}</div>
-			</div>
+			<h3>Pasajeros (sugar syntax)</h3>
+			<ul>
+				<!-- sintax para recorrer un Array -->
+				<!-- Angular no da un variable que se llama 'index' -->
+				<li *ngFor="let passenger of passengers; let i = index;">
+					{{ i }}: {{ passenger.fullname }}
+				</li>
+			</ul>
 			
 		</div>
 	`
 })
 export class AppComponent {
-  name: string = "";
-
-  handleChange(value: string) {
-    this.name = value;
-  }
+  passengers: Passenger[] = [{
+		id: 1,
+		fullname: 'Hector',
+		checkedIn: true
+	},{
+		id: 2,
+		fullname: 'Raquel',
+		checkedIn: true
+	},{
+		id: 3,
+		fullname: 'Maria',
+		checkedIn: false
+	},{
+		id: 4,
+		fullname: 'Juan',
+		checkedIn: false
+	},{
+		id: 5,
+		fullname: 'Peper',
+		checkedIn: true
+	}];
 }
 
 /**
- * *ngIf es una DIRECTIVA que nos permite de una manera muy fácil
- * mostrar/ocultar contenido dependiendo de una condición	.
+ * *ngFor es una DIRECTIVA que nos permite de una manera muy fácil
+ * recorrer un array.
+ * Vamos a usar una Interface para crear el tipo de Dato personalizado
+ * en TS con diferentes restricciones.
+ * 
  *  
  */
