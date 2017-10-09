5ffd48f1013299f9cf1399892c2ff4bba651274c
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-10-09 19:53:43 +0200

    12 - ngStyle and style bindings
    Como modificar estilos dinamicamente accediendo a las propiedades del elemento igual que en JS y evaluando una expresión.

--- app/app.component.ts
+++ app/app.component.ts
@@ -15,7 +15,6 @@ interface Passenger {
 			<h3>Pasajeros (sugar syntax)</h3>
 			<ul>
 				<li *ngFor="let passenger of passengers; let i = index;">
-					<!-- sintax para añadir una Clase -->
 					<span 
 						class="status"
 						[class.checked-in]="passenger.checkedIn"></span>
@@ -23,17 +22,48 @@ interface Passenger {
 				</li>
 			</ul>
 
+			<h3>Pasajeros</h3>
 			<ul>
-			<li *ngFor="let passenger of passengers; let i = index;">
-				<!-- sintax para añadir varias Clases a través de un Obj -->
-				<span 
-					class="status"
-					[ngClass]="{ 
-						'checked-in' : passenger.checkedIn,
-						'checked-out': !passenger.checkedIn }"></span>
-				{{ i }}: {{ passenger.fullname }}
-			</li>
-		</ul>
+				<li *ngFor="let passenger of passengers; let i = index;">
+					<span 
+						class="status"
+						[ngClass]="{ 
+							'checked-in' : passenger.checkedIn,
+							'checked-out': !passenger.checkedIn }"></span>
+					{{ i }}: {{ passenger.fullname }}
+				</li>
+			</ul>
+
+			<h3>Pasajeros (style)</h3>
+			<ul>
+				<li *ngFor="let passenger of passengers; let i = index;">
+					<!-- sintax para añadir Estilos dinámicamente -->
+					<span 
+						class="status"
+						[style.backgroundColor]="(passenger.checkedIn ? 'green' : 'red') "></span>
+					{{ i }}: {{ passenger.fullname }}
+				</li>
+			</ul>
+
+			<h3>Pasajeros (ngStyle)</h3>
+			<ul>
+				<li *ngFor="let passenger of passengers; let i = index;">
+					<!-- sintax para añadir Estilos dinámicamente -->
+					<span 
+						class="status"
+						[ngStyle]="{
+							backgroundColor: (passenger.checkedIn ? 'green' : 'red'),
+							color: (passenger.checkedIn ? 'yellowgreen' : 'orange')
+						} "></span>
+					<span
+						[ngStyle]="{
+							backgroundColor: (passenger.checkedIn ? 'green' : 'red'),
+							color: (passenger.checkedIn ? 'yellowgreen' : 'orange')
+						}">	
+					{{ i }}: {{ passenger.fullname }}
+					</span>
+				</li>
+			</ul>
 			
 		</div>
 	`
@@ -63,8 +93,10 @@ export class AppComponent {
 }
 
 /**
- * ngClass nos permite añadir clases evaluando una expresión
- * 
- * 
- *  
+ * [style.propiedad] = "expr" nos permite acceder a las propiedades de cada Elemento
+ * igual que en JS y evaluar una Condición para modificarla
+ * dinamicamente.
+ * [ngStyle] = { propiedad : "expr" } nos permite acceder a las propiedades de cada Elemento
+ * igual que en JS pero esta vez como un Objeto y evaluar varias 
+ * Condiciones para modificarlas dinámicamente.
  */
