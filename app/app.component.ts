f5fabc4b8f4fe45f371e37ee549fb5832b21e850
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-10-22 18:27:02 +0200

    16 Creating a container (smart) component
    Creación de un 'Smart Container' y una carpeta independiente para las 'Interface' que hace referencia al modelo en la BBDD.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,79 +1,15 @@
 import { Component } from "@angular/core";
 
-interface child {
-	name: string,
-	age: number
-}
-interface Passenger {
-	id: number,
-	fullname: string,
-	checkedIn: boolean,
-	// Con | null podemos hacerlo Opcional también.
-	checkInDate: number | null,
-	// El operador ? hace que el Dato sea opcional. 
-	children: child[] | null
-}
-
 @Component({
   selector: "app-root",
   styleUrls: ["app.component.scss"],
   template: `
 		<div class="app">
-
-			<h3>Pasajeros (sugar syntax)</h3>
-			<ul>
-				<li *ngFor="let passenger of passengers; let i = index;">
-					<span 
-						class="status"
-						[class.checked-in]="passenger.checkedIn"></span>
-					{{ i }}: {{ passenger.fullname }}
-					<p>{{ passenger | json }}</p>
-					<div class="date">
-						Check in date: {{ passenger.checkInDate ? (passenger.checkInDate | date: 'y MMM' | uppercase) : 'Not checked in...' }}
-					</div>
-					<div class="children">
-						<!-- El operador ? hace que el Dato sea opcional. -->
-						Children: {{ passenger.children?.length || 0 }}
-					</div>
-				</li>
-			</ul>
-			
+			<passenger-dashboard></passenger-dashboard>
 		</div>
 	`
 })
-export class AppComponent {
-  passengers: Passenger[] = [{
-		id: 1,
-		fullname: 'Hector',
-		checkedIn: true,
-		checkInDate: 1490742000000,
-		children: [{ name: 'Ted', age: 12},{ name: 'Chloe', age: 7}]
-	},{
-		id: 2,
-		fullname: 'Raquel',
-		checkedIn: true,
-		checkInDate: 1890742000000,
-		children: null
-	},{
-		id: 3,
-		fullname: 'Maria',
-		checkedIn: false,
-		checkInDate: null,
-		children: [{ name: 'Ted', age: 12},{ name: 'Chloe', age: 7}]
-	},{
-		id: 4,
-		fullname: 'Juan',
-		checkedIn: false,
-		checkInDate: null,
-		children: [{ name: 'Ted', age: 12},{ name: 'Chloe', age: 7}]
-	},{
-		id: 5,
-		fullname: 'Peper',
-		checkedIn: true,
-		checkInDate: 1290742000000,
-		children: null
-	}];
-}
+export class AppComponent {}
 
 /**
  * El operador ? nos permite chequear una variable y devuelve
---
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -0,0 +1,61 @@
+import { Component } from "@angular/core";
+
+import { Passenger } from "../models/passenger.interface";
+
+@Component({
+	selector: 'passenger-dashboard',
+	styleUrls: ['passenger-dashboard.component.scss'],
+	template: `
+		<div>
+			<h3>Passenger Dashboard</h3>
+			<ul>
+				<li *ngFor="let passenger of passengers; let i = index;">
+					<span 
+						class="status"
+						[class.checked-in]="passenger.checkedIn"></span>
+					{{ i }}: {{ passenger.fullname }}
+					<div class="date">
+						Check in date: {{ passenger.checkInDate ? (passenger.checkInDate | date: 'y MMM' | uppercase) : 'Not checked in...' }}
+					</div>
+					<div class="children">
+						<!-- El operador ? hace que el Dato sea opcional. -->
+						Children: {{ passenger.children?.length || 0 }}
+					</div>
+				</li>
+			</ul>
+		</div>
+	`
+})
+export class PassengerDashboardComponent {
+	passengers: Passenger[] = [{
+		id: 1,
+		fullname: 'Hector',
+		checkedIn: true,
+		checkInDate: 1490742000000,
+		children: [{ name: 'Ted', age: 12},{ name: 'Chloe', age: 7}]
+	},{
+		id: 2,
+		fullname: 'Raquel',
+		checkedIn: true,
+		checkInDate: 1890742000000,
+		children: null
+	},{
+		id: 3,
+		fullname: 'Maria',
+		checkedIn: false,
+		checkInDate: null,
+		children: [{ name: 'Ted', age: 12},{ name: 'Chloe', age: 7}]
+	},{
+		id: 4,
+		fullname: 'Juan',
+		checkedIn: false,
+		checkInDate: null,
+		children: [{ name: 'Ted', age: 12},{ name: 'Chloe', age: 7}]
+	},{
+		id: 5,
+		fullname: 'Peper',
+		checkedIn: true,
+		checkInDate: 1290742000000,
+		children: null
+	}];
+}
